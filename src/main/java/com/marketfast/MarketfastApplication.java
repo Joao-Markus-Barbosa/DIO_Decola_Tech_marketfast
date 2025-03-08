d4bb84fde13f528c86e02e366ce645a1ca73b658
João Markus <markusjoaomarkus99@gmail.com>
2025-03-08 19:11:33 -0300

    Bugfix CORS

--- src/main/java/com/marketfast/MarketfastApplication.java
+++ src/main/java/com/marketfast/MarketfastApplication.java
@@ -1,9 +1,13 @@
 package com.marketfast;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+
+@OpenAPIDefinition(servers = {@Server(url="/", description = "Default Server URL")})
 @SpringBootApplication
 @EntityScan(basePackages = {
 		"com.marketfast.domain.cliente.model",
