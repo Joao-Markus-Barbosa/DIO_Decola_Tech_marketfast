13d87301e9300c4b657e5388a5b3cddf4a08dd75
João Markus Barbosa <153394811+Joao-Markus-Barbosa@users.noreply.github.com>
2025-03-08 19:39:22 -0300

    Update MarketfastApplication.java

--- src/main/java/com/marketfast/MarketfastApplication.java
+++ src/main/java/com/marketfast/MarketfastApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@OpenAPIDefinition(servers = {@Server(url="/", description = "Default Server URL")})
 @EntityScan(basePackages = {
 		"com.marketfast.domain.cliente.model",
 		"com.marketfast.domain.pedido.model",
